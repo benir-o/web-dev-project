9353aecd0e52be848c4642e863a92fabdb80a1c1
benir-o <beniromenda@gmail.com>
2025-06-18 22:24:04 +0300

    Some housekeeping updates

--- Benir's work/test.js
+++ Benir's work/test.js
@@ -1,11 +1,13 @@
 let userTokenBalance=0;
 let userID;
+let kesAmount;
 const tokenUnit=20.57;
 let initialTokens=0;
+let tokenAmount;
 
 
 const checkBalanceButton=document.querySelector('#checkBalance');
-const buyTokensButton=document.querySelector('#testButton');
+const buyTokensButton=document.querySelector('#buyTokens');
 const viewHistoryButton=document.querySelector('#viewHistory');
 const viewUsageButton=document.querySelector('#viewUsage');
 const sendTokensButton=document.querySelector('#sendTokens');
@@ -26,14 +28,20 @@ viewUsageButton.onclick=viewUsage;
 sendTokensButton.onclick=sendTokens;
 
 function checkUserBalance(){}
-function buyTokens(Kshamount){
-    if (Kshamount<tokenUnit){
-        //Tell user invalid input
-    }else{
-        let token=Kshamount/tokenUnit;
-        initialTokens+=token;
+
+function buyTokens(event) {
+     // Prevent form submission
+    const kshAmountInput = document.getElementById('kenyanAmount');
+    const kshAmountValue = Number(kshAmountInput.value);
+    if (kshAmountValue < tokenUnit) {
+        event.preventDefault();
+        buyTokensButton.innerText = "Invalid";
+        alert("Tokens are purchased from 20 shillings and above");
+    } else {
+        tokenAmount=kshAmountValue/tokenUnit;
+        initialTokens+=tokenAmount;
+        buyTokensButton.innerText = "Success!";
     }
-    return initialTokens;
 }
 function viewHistory(){}
 function viewUsage(){}
